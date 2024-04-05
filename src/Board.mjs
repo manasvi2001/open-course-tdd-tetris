89b45475e0e29016246f7c5a7000c47ad1d3c9c6
NC24568_Manasvi <nc24568_manasvi@Ninja-ITs-MacBook-Air.local>
2024-04-05 20:37:41 +0530

    fixed test

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,19 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = "...\n...\n...\n";
   }
 
   toString() {
-    return "TODO";
+    return this.board;
+  }
+
+  drop(block) {
+    this.board = ".X.\n...\n...\n";
   }
 }
